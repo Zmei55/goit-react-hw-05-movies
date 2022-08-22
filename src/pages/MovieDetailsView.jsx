cca0a4bef3111dc9a8815a07dd49f7a83bd0f82f
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-08-22 15:49:29 +0200

    Button "Back", useLocation

--- src/pages/MovieDetailsView.jsx
+++ src/pages/MovieDetailsView.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import * as movieAPI from '../services/movie-api';
 import { Button } from '../components/Button';
 import { MovieDetails } from '../components/MovieDetails';
@@ -7,6 +7,8 @@ import { MovieCastView } from '../pages/MovieCastView';
 import { MovieReviewsView } from '../pages/MovieReviewsView';
 
 export function MovieDetailsView() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const { movieId } = useParams();
   const [movie, setMovie] = useState('');
 
@@ -14,10 +16,13 @@ export function MovieDetailsView() {
     movieAPI.fetchMovieById(movieId).then(setMovie);
   }, [movieId]);
 
+  function onGoBack() {
+    navigate(location?.state?.from ?? '/');
+  }
+
   return (
     <>
-      {/* <Button>Back</Button> */}
-      <Button text="Back" />
+      <Button text="Back" onClick={onGoBack} />
       <MovieDetails movie={movie} />
       <hr />
       <MovieCastView />
