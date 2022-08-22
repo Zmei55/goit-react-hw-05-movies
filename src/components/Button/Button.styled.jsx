bfec913c6f33565adfae3b7aa66f7c7951ce0fc6
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-08-22 14:56:40 +0200

    Button "Back"

---
+++ src/components/Button/Button.styled.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Btn = styled.button`
+  padding: 8px 16px;
+  margin-bottom: 16px;
+`;
--- src/pages/MovieDetailsView.jsx
+++ src/pages/MovieDetailsView.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import * as movieAPI from '../services/movie-api';
+import { Button } from '../components/Button';
 import { MovieDetails } from '../components/MovieDetails';
 import { MovieCastView } from '../pages/MovieCastView';
 import { MovieReviewsView } from '../pages/MovieReviewsView';
@@ -15,12 +16,11 @@ export function MovieDetailsView() {
 
   return (
     <>
+      {/* <Button>Back</Button> */}
+      <Button text="Back" />
       <MovieDetails movie={movie} />
-
       <hr />
-
       <MovieCastView />
-
       <MovieReviewsView />
     </>
   );
